5a865af011d9a3066d6b996d1e9d01d86abb0d70
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-12-30 19:27:21 -0500

    updated http parser to fix segfaults, still not getting headers

--- http_parser.c
+++ http_parser.c
@@ -5,61 +5,61 @@
 
 HttpRequest parse_request(const char *request) {
     HttpRequest parsed_request;
+    memset(&parsed_request, 0, sizeof(HttpRequest));  // Initialize all fields to zero
 
     char buffer[1024];
     strncpy(buffer, request, sizeof(buffer) - 1);
     buffer[sizeof(buffer) - 1] = '\0';
 
-    char *line = strtok(buffer, "\n");
-    
-    // take the method, path, and version from line 1
-    int location = 0;
-    char *sub_line = strtok(line, " ");
-
-    while(sub_line != NULL) {
-        sub_line = strtok(NULL, " ");
-        location++;
-        switch (location)
-        {
-        case 1:
-            strncpy(parsed_request.method, sub_line, sizeof(parsed_request.method) - 1);
-            parsed_request.method[sizeof(parsed_request.method) - 1] = '\0';
-            break;
-        case 2:
-            strncpy(parsed_request.path, sub_line, sizeof(parsed_request.path) - 1);
-            parsed_request.path[sizeof(parsed_request.path) - 1] = '\0';
-            break;
-        case 3:
-            strncpy(parsed_request.version, sub_line, sizeof(parsed_request.version) - 1);
-            parsed_request.version[sizeof(parsed_request.version) - 1] = '\0';
-            break;
-        default:
-            break;
-        }
+    // Get the first line
+    char *line = strtok(buffer, "\r\n");
+    if (line == NULL) {
+        return parsed_request;
     }
 
-    // Split the rest of the lines into key-value pairs to be stored in headers
-    int header_index = 0; // Keep track of the current header index
-    while ((line = strtok(NULL, "\n")) != NULL) {
-        // Split line on ": "
-        char *sub_key = strtok(line, ": ");
-        char *sub_val = strtok(NULL, "");
+    // Parse first line (METHOD PATH VERSION)
+    char *method = strtok(line, " ");
+    char *path = strtok(NULL, " ");
+    char *version = strtok(NULL, " \r\n");
 
-        // Ensure both key and value are non-NULL
-        if (sub_key != NULL && sub_val != NULL && header_index < 10) {
-            // Copy key into headers[header_index][0]
-            strncpy(parsed_request.headers[header_index][0], sub_key, sizeof(parsed_request.headers[header_index][0]) - 1);
-            parsed_request.headers[header_index][0][sizeof(parsed_request.headers[header_index][0]) - 1] = '\0'; // Null-terminate
-
-            // Copy value into headers[header_index][1]
-            strncpy(parsed_request.headers[header_index][1], sub_val, sizeof(parsed_request.headers[header_index][1]) - 1);
-            parsed_request.headers[header_index][1][sizeof(parsed_request.headers[header_index][1]) - 1] = '\0'; // Null-terminate
-
-            header_index++; // Move to the next header slot
-        }
+    // Safely copy if tokens exist
+    if (method) {
+        strncpy(parsed_request.method, method, sizeof(parsed_request.method) - 1);
+        parsed_request.method[sizeof(parsed_request.method) - 1] = '\0';
+    }
+    if (path) {
+        strncpy(parsed_request.path, path, sizeof(parsed_request.path) - 1);
+        parsed_request.path[sizeof(parsed_request.path) - 1] = '\0';
+    }
+    if (version) {
+        strncpy(parsed_request.version, version, sizeof(parsed_request.version) - 1);
+        parsed_request.version[sizeof(parsed_request.version) - 1] = '\0';
     }
 
+    // Parse headers
+    int header_index = 0;
+    while ((line = strtok(NULL, "\r\n")) != NULL && header_index < 10) {
+        // Skip empty lines
+        if (strlen(line) == 0) {
+            continue;
+        }
+
+        char *key = strtok(line, ":");
+        char *value = strtok(NULL, "\r\n");
+
+        if (key && value) {
+            // Skip leading whitespace in value
+            while (*value == ' ') value++;
+            
+            strncpy(parsed_request.headers[header_index][0], key, sizeof(parsed_request.headers[header_index][0]) - 1);
+            parsed_request.headers[header_index][0][sizeof(parsed_request.headers[header_index][0]) - 1] = '\0';
+            
+            strncpy(parsed_request.headers[header_index][1], value, sizeof(parsed_request.headers[header_index][1]) - 1);
+            parsed_request.headers[header_index][1][sizeof(parsed_request.headers[header_index][1]) - 1] = '\0';
+            
+            header_index++;
+        }
+    }
 
     return parsed_request;
-
 }
